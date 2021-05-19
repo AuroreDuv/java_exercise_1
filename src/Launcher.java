922e9679fa0fe0e09f874266b3e2aca5dd9ffb0f
Aurore DUVERNOY <aduvernoy@visualstorm.fr>
2021-05-19 22:54:37 +0200

    Launcher ok

---
+++ src/Launcher.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Launcher {
+	public static void main(String args[]) {
+		System.out.println("Hello");
+
+		
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+
+    if(input.equals("quit"))
+    {
+		  System.exit(0);
+    }
+    else
+    {
+		  System.out.println("Unknown command");
+    }
+	}
+}
